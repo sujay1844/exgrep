4fe6f3b954b1747e446f83d24b5c09cdcc2cbcbc
Sujay R <68642484+sujay1844@users.noreply.github.com>
2024-05-20 21:05:14 +0530

    Refactor string match code to main function

--- lib/process.ex
+++ lib/process.ex
@@ -1,41 +1,34 @@
-defmodule Exgrep.Process do
-  @spec process_input(String.t(), String.t()) :: [{String.t(), integer()}]
-  def process_input(input_file, pattern) do
-    chunk_size = 2
+defmodule FileHandler do
+  @spec read(String.t()) :: File.Stream.t()
+  def read(input_file) do
+    Path.expand(input_file)
+    |> File.stream!(:line)
+  end
+end
 
-    input_file
-    |> read_file_as_stream()
-    |> trim_lines_and_add_index()
+defmodule Processor do
+  @spec process_input(
+          File.Stream.t(),
+          (String.t() -> boolean()),
+          integer()
+        ) :: [{String.t(), integer()}]
+  def process_input(file_stream, match_fn, chunk_size) do
+    file_stream
+    |> Stream.map(&String.trim/1)
+    |> Stream.with_index()
     |> Stream.chunk_every(chunk_size)
-    |> Task.async_stream(fn batch -> filter_lines_matching_pattern(batch, pattern) end)
+    |> Task.async_stream(fn batch -> filter_lines_matching_pattern(batch, match_fn) end)
     |> Enum.map(fn {:ok, result} -> result end)
     |> List.flatten()
   end
 
-  @spec read_file_as_stream(String.t()) :: File.Stream.t()
-  defp read_file_as_stream(input_file) do
-    Path.expand(input_file)
-    |> File.stream!(:line)
-  end
-
-  @spec trim_lines_and_add_index(File.Stream.t()) :: Stream.t({String.t(), integer()})
-  defp trim_lines_and_add_index(input_stream) do
-    input_stream
-    |> Stream.map(&String.trim/1)
-    |> Stream.with_index()
-  end
-
-  @spec filter_lines_matching_pattern([{String.t(), integer()}], String.t()) :: [
-          {String.t(), integer()}
-        ]
-  defp filter_lines_matching_pattern(batch, pattern) do
+  @spec filter_lines_matching_pattern(
+          Stream.t({String.t(), integer()}),
+          (String.t() -> boolean())
+        ) :: Stream.t({String.t(), integer()})
+  defp filter_lines_matching_pattern(batch, match_fn) do
     batch
-    |> Enum.filter(fn {line, _} -> line_contains_pattern?(line, pattern) end)
+    |> Enum.filter(fn {line, _} -> match_fn.(line) end)
     |> Enum.map(fn {line, index} -> {line, index} end)
-  end
-
-  @spec line_contains_pattern?(String.t(), String.t()) :: boolean()
-  defp line_contains_pattern?(line, pattern) do
-    String.contains?(line, pattern)
   end
 end
